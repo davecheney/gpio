bac54fdb14bcdd53228c092653c7b08ad144247b
Dave Cheney <dave@cheney.net>
2013-07-23 06:59:38 +0000

    Merge in my old gpio_linux /sys implementation.

--- gpio_linux.go
+++ gpio_linux.go
@@ -2,8 +2,10 @@ package gpio
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
-	"strconv"
+	"path/filepath"
+	"strings"
 )
 
 //By default, pins 14 and 15 boot to UART mode, so they are going to be ignored for now.
@@ -29,11 +31,9 @@ const (
 )
 
 const (
-	exportPath      = "/sys/class/gpio/export"
-	unexportPath    = "/sys/class/gpio/unexport"
-	gpioPathPrefix  = "/sys/class/gpio/gpio"
-	modePathSuffix  = "/direction"
-	valuePathSuffix = "/value"
+	gpiobase     = "/sys/class/gpio"
+	exportPath   = "/sys/class/gpio/export"
+	unexportPath = "/sys/class/gpio/unexport"
 )
 
 var (
@@ -52,70 +52,53 @@ type pin struct {
 
 // OpenPin exports the pin, creating the virtual files necessary for interacting with the pin.
 // It also sets the mode for the pin, making it ready for use.
-func OpenPin(number int, mode Mode) (Pin, error) {
-	numString := strconv.Itoa(number)
-	p := &pin{
-		number:        number,
-		numberAsBytes: []byte(numString),
-		modePath:      fmt.Sprintf("%s%s%s", gpioPathPrefix, numString, modePathSuffix),
-	}
-
+func OpenPin(n int, mode Mode) (Pin, error) {
 	// export this pin to create the virtual files on the system
-	exportFile, err := os.OpenFile(exportPath, os.O_WRONLY, 0200)
+	pinBase, err := expose(n)
 	if err != nil {
 		return nil, err
 	}
-	defer exportFile.Close()
-	if _, err := exportFile.Write(p.numberAsBytes); err != nil {
+	value, err := os.OpenFile(filepath.Join(pinBase, "value"), os.O_RDWR, 0600)
+	if err != nil {
 		return nil, err
 	}
+	p := &pin{
+		number:    n,
+		modePath:  filepath.Join(pinBase, "direction"),
+		valueFile: value,
+	}
 	p.SetMode(mode)
-	valueFile, err := os.Create(fmt.Sprintf("%s%s%s", gpioPathPrefix, numString, valuePathSuffix))
-	p.valueFile = valueFile
-	p.err = err
-
 	return p, nil
-
 }
 
 // write opens a file for writing, writes the byte slice to it and closes the
 // file.
-func write(bytes []byte, path string) error {
-	file, err := os.Create(path)
+func write(buf []byte, path string) error {
+	file, err := os.OpenFile(filepath.Join(gpiobase, path), os.O_WRONLY, 0600)
 	if err != nil {
 		return err
 	}
-
-	file.Write(bytes)
-
+	if _, err := file.Write(buf); err != nil {
+		return err
+	}
 	return file.Close()
 }
 
 // read opens a file for reading, reads the bytes slice from it and closes the file.
-func read(bytes *[]byte, path string) error {
-	file, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-
-	file.Read(*bytes)
-
-	return file.Close()
+func read(path string) ([]byte, error) {
+	return ioutil.ReadFile(path)
 }
 
 // Close destroys the virtual files on the filesystem, unexporting the pin.
 func (p *pin) Close() error {
-	p.valueFile.Close()
-	err := write(p.numberAsBytes, unexportPath)
-	p.err = err
-	return err
+	return writeFile(filepath.Join(gpiobase, "unexport"), "%d", p.number)
 }
 
 // Mode retrieves the current mode of the pin.
 func (p *pin) Mode() Mode {
-	bytes := make([]byte, 3)
-	p.err = read(&bytes, p.modePath)
-	return Mode(bytes)
+	var mode string
+	mode, p.err = readFile(p.modePath)
+	return Mode(mode)
 }
 
 // SetMode sets the mode of the pin.
@@ -136,8 +119,7 @@ func (p *pin) Clear() {
 // Get retrieves the current pin level.
 func (p *pin) Get() bool {
 	bytes := make([]byte, 1)
-	p.valueFile.Read(bytes)
-
+	_, p.err = p.valueFile.Read(bytes)
 	return bytes[0] != 0
 }
 
@@ -154,4 +136,24 @@ func (p *pin) Wait(condition bool) {
 // Err returns the last error encountered.
 func (p *pin) Err() error {
 	return p.err
+}
+
+func expose(pin int) (string, error) {
+	err := writeFile(filepath.Join(gpiobase, "export"), "%d", pin)
+	return filepath.Join(gpiobase, fmt.Sprintf("gpio%d", pin)), err
+}
+
+func writeFile(path string, format string, args ...interface{}) error {
+	f, err := os.OpenFile(path, os.O_WRONLY, 0777)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = fmt.Fprintf(f, format, args...)
+	return err
+}
+
+func readFile(path string) (string, error) {
+	buf, err := ioutil.ReadFile(path)
+	return strings.TrimSpace(string(buf)), err
 }
