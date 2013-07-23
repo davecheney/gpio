a4a9569e5872560e45425e8812e64b5dc6fb6078
Tyler Bunnell <tylerbunnell@gmail.com>
2013-07-22 18:49:43 -0600

    Cleaning things up a bit

--- README.md
+++ README.md
@@ -6,8 +6,6 @@ GPIO for Go
 testing
 -------
 
-The `linux` implementation uses the /sys/class/gpio kernel driver. The files in this directory are owned by root, so you cannot test as a mortal user. To run your tests, try this method
-
-    go test -c && ./gpio.test -test.v
+Please run the 'blink' example found under examples/blink. Proper tests will be added soon.
 
 
--- gpio_linux.go
+++ gpio_linux.go
@@ -1,7 +1,46 @@
 package gpio
 
 import (
+	"fmt"
+	"os"
 	"strconv"
+)
+
+//Constants
+
+//By default, pins 14 and 15 boot to UART mode, so they are going to be ignored for now.
+//We can add them in later as necessary.
+
+const (
+	GPIO0  = 0
+	GPIO1  = 1
+	GPIO2  = 2
+	GPIO3  = 3
+	GPIO4  = 4
+	GPIO7  = 7
+	GPIO8  = 8
+	GPIO9  = 9
+	GPIO10 = 10
+	GPIO11 = 11
+	GPIO17 = 17
+	GPIO18 = 18
+	GPIO22 = 22
+	GPIO23 = 23
+	GPIO24 = 24
+	GPIO25 = 25
+)
+
+const (
+	exportPath      = "/sys/class/gpio/export"
+	unexportPath    = "/sys/class/gpio/unexport"
+	gpioPathPrefix  = "/sys/class/gpio/gpio"
+	modePathSuffix  = "/direction"
+	valuePathSuffix = "/value"
+)
+
+var (
+	BytesSet   = []byte{'1'}
+	BytesClear = []byte{'0'}
 )
 
 // pin represents a GPIO pin.
@@ -13,73 +52,84 @@ type pin struct {
 	err           error  //the last error
 }
 
-func OpenPin(number int) (Pin, error) {
+// OpenPin exports the pin, creating the virtual files necessary for interacting with the pin.
+// It also sets the mode for the pin, making it ready for use.
+func OpenPin(number int, mode Mode) (Pin, error) {
 	numString := strconv.Itoa(number)
 	p := &pin{
 		number:        number,
 		numberAsBytes: []byte(numString),
-		modePath:      MergeStrings(gpioPathPrefix, numString, modePathSuffix),
-		valuePath:     MergeStrings(gpioPathPrefix, numString, valuePathSuffix),
+		modePath:      fmt.Sprintf("%s%s%s", gpioPathPrefix, numString, modePathSuffix),
+		valuePath:     fmt.Sprintf("%s%s%s", gpioPathPrefix, numString, valuePathSuffix),
 	}
 
 	// export this pin to create the virtual files on the system
 	err := write(p.numberAsBytes, exportPath)
 	p.err = err
 
+	if err != nil {
+		p.SetMode(mode)
+	}
+
 	return p, err
 
 }
 
+// write opens a file for writing, writes the byte slice to it and closes the
+// file.
 func write(bytes []byte, path string) error {
-	file, err := OpenForWrite(path)
+	file, err := os.Create(path)
 	if err != nil {
 		return err
 	}
 
 	file.Write(bytes)
 
-	return Close(file)
+	return file.Close()
 }
 
+// read opens a file for reading, reads the bytes slice from it and closes the file.
 func read(bytes *[]byte, path string) error {
-	file, err := OpenForRead(path)
+	file, err := os.Open(path)
 	if err != nil {
 		return err
 	}
 
 	file.Read(*bytes)
 
-	return Close(file)
+	return file.Close()
 }
 
-// Close destroys the virtual FD on the filesystem
+// Close destroys the virtual files on the filesystem, unexporting the pin.
 func (p *pin) Close() error {
 	err := write(p.numberAsBytes, unexportPath)
 	p.err = err
 	return err
 }
 
+// Mode retrieves the current mode of the pin.
 func (p *pin) Mode() Mode {
 	bytes := make([]byte, 3)
 	p.err = read(&bytes, p.modePath)
 	return Mode(bytes)
 }
 
+// SetMode sets the mode of the pin.
 func (p *pin) SetMode(mode Mode) {
 	p.err = write([]byte(mode), p.modePath)
 }
 
-// Set will be replaced by a /dev/mem access
+// Set sets the pin level high.
 func (p *pin) Set() {
 	p.err = write(BytesSet, p.valuePath)
 }
 
-// Clear will be replaced by a /dev/mem access
+// Clear sets the pin level low.
 func (p *pin) Clear() {
 	p.err = write(BytesClear, p.valuePath)
 }
 
-// Get will be replaced by a /dev/mem access
+// Get retrieves the current pin level.
 func (p *pin) Get() bool {
 	bytes := make([]byte, 1)
 	p.err = read(&bytes, p.valuePath)
@@ -87,14 +137,17 @@ func (p *pin) Get() bool {
 	return bytes[0] != 0
 }
 
+// Watch waits for the edge level to be triggered and then calls the callback
 func (p *pin) Watch(callback IRQEvent) {
 	panic("Watch is not yet implemented!")
 }
 
+// Wait blocks while waits for the pin state to match the condition, then returns.
 func (p *pin) Wait(condition bool) {
 	panic("Wait is not yet implemented!")
 }
 
+// Err returns the last error encountered.
 func (p *pin) Err() error {
 	return p.err
 }
