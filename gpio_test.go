dcda91fc69312e2a89af937a25c0d55c25c41b97
Dave Cheney <dave@cheney.net>
2013-07-23 02:50:48 +0000

    Added test helpers

---
+++ gpio_test.go
@@ -0,0 +1,22 @@
+package gpio
+
+// test helpers
+
+import (
+	"os"
+	"testing"
+)
+
+// checkRoot checks if the user is root, and skips the test if not.
+func checkRoot(t *testing.T) {
+	if os.Geteuid() != 0 {
+		t.Skip("test requires root privs")
+	}
+}
+
+// checkNotRoot checks that the user is NOT root, and skips the test otherwise.
+func checkNotRoot(t *testing.T) {
+	if os.Geteuid() == 0 {
+		t.Skip("skipping test for root user")
+	}
+}
